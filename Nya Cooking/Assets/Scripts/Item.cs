5bc321d628a668fa6bfac2cdcce78f62aa604083
LadyRavenFire <kilkust@gmail.com>
2018-06-05 18:25:43 +0500

    item script
    item script

---
+++ Nya Cooking/Assets/Scripts/Item.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+
+    public string ItemName;
+    public int ItemID;
+    public Texture2D ItemIcon;
+    public StateOfRare stateOfRare;
+    public StateOfIncision stateOfIncision;
+
+    public enum StateOfRare
+    {
+        Raw,
+        Fried,
+        Burnt
+    }
+
+    public enum StateOfIncision
+    {
+        Whole,
+        Cutted,
+        Diced,
+        Stripped
+
+    }
+
+}
