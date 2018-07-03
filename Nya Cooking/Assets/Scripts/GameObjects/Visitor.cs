a96b81657d60d4d28ade06b3d25ff7c55b21ec16
Alex J <alexander.aj.ranger@gmail.com>
2018-07-03 18:15:55 +0500

    some visitor thoughts

--- Nya Cooking/Assets/Scripts/GameObjects/Visitor.cs
+++ Nya Cooking/Assets/Scripts/GameObjects/Visitor.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Visitor : MonoBehaviour {
+public class Visitor : MonoBehaviour
+{
+    private Item Wanted;
+    private float TimeToBlow;
+    // 3 текстуры
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    Visitor v1 = GameObject.FindWithTag("Visitor0").GetComponent<Visitor>();
+	    var v2 = Instantiate(v1);
 	}
 	
 	// Update is called once per frame
