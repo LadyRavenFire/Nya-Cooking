ce3268ed46a8732bf41525c80f5ac54e645a9763
Alex J <alexander.aj.ranger@gmail.com>
2018-07-03 18:15:38 +0500

    Some recepies thoughts

--- Nya Cooking/Assets/Scripts/Managers/Recipes.cs
+++ Nya Cooking/Assets/Scripts/Managers/Recipes.cs
@@ -1,11 +1,29 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class Recipes : MonoBehaviour
 {
 
+    public class LevelReceipes
+    {
+        public List<Receipe> Recepies;
+        public List<Receipe> SecretReceipes;
+    }
+
     private ItemDataBase _db;
+
+    /// <summary>
+    /// База знаний абсолютно ВСЕХ рецептов, которые могут быть в игре
+    /// </summary>
     public List<Receipe> Receipes;
+   /// <summary>
+   /// База знаний абсолютно ВСЕХ секретных рецептов, которые могут быть в игре
+   /// </summary>
+    public List<SecretReceipe> SecretReceipes;
+
+    public Dictionary<int, List<Receipe>> CampaignRecepies;
+    public Dictionary<int, List<Receipe>> EndlessRecepies;
 
     void Start()
     {
@@ -31,7 +49,6 @@ public class Recipes : MonoBehaviour
                 }
             },
             Result = _db.Generate(Item.Name.Sandwich, Item.StateOfIncision.Whole, Item.StateOfPreparing.Raw, false)
-
         };
         Receipe recipe2 = new Receipe()
         {
@@ -61,5 +78,11 @@ public class Recipes : MonoBehaviour
     {
         public List<Item> Ingridients { get; set; }
         public Item Result { get; set; }
+    }
+
+    public class SecretReceipe
+    {
+        public Receipe Receipe { get; set; }
+        public bool IsOpened { get;set; }
     }
 }
